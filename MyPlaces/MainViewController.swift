7a72ae6103871455dc3bf21ccb17cfa365f471e8
ubar256 <ruslan0809.89@mail.ru>
2021-02-21 22:49:01 +0300

    New Place Adding, Saving, Images features

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class MainViewController: UITableViewController {
     
     
-    let places = Place.getPlaces()
+    var places = Place.getPlaces()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +29,19 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        cell.nameLabel?.text = places[indexPath.row].name
-        cell.locationLabel.text = places[indexPath.row].location
-        cell.typeLabel.text = places[indexPath.row].type
-        cell.imageOfPlace?.image = UIImage(named: places[indexPath.row].name)
+        let place = places[indexPath.row]
+        
+        cell.nameLabel?.text = place.name
+        cell.locationLabel.text = place.location
+        cell.typeLabel.text = place.type
+        
+        if place.image == nil {
+            cell.imageOfPlace?.image = UIImage(named: place.restaurantImage!)
+        } else {
+            cell.imageOfPlace.image = place.image
+        }
+        
+      
         
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height/2
         cell.imageOfPlace?.clipsToBounds = true
@@ -50,5 +59,12 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func unwindSegue(_segue:UIStoryboardSegue) {
+        guard let newPlaceVC = _segue.source as? NewPlaceViewController else {return}
+        newPlaceVC.saveNewPlace()
+        places.append(newPlaceVC.newPlace!)
+        tableView.reloadData()
+    }
 
+    
 }
--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -5,20 +5,21 @@
 //  Created by Руслан Кулийбаев on 19.02.2021.
 //
 
-import Foundation
+import UIKit
 
 struct Place {
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var image : UIImage?
+    var restaurantImage: String?
     
     static let restaurantNames = ["Hitch", "Subzero", "Фильтр", "Балкон", "Frank", "Мясная лавка", "Абрикосов", "Pshaveli", "Tokyo city", "Цех 85", "Kwakker", "Kontakt bar", "Mishka bar"]
     
     static func getPlaces() -> [Place] {
         var places = [Place] ()
         for place in restaurantNames {
-            places.append(Place(name: place, location: "СПб", type: "Ресторан", image: place))
+            places.append(Place(name: place, location: "СПб", type: "Ресторан",image: nil, restaurantImage: place))
         }
         return places
     }
